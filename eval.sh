a3a914f6b329ff532ede3a74230ed5cd8d0eefb4
Alexandre Marques <alexandre@neuralmagic.com>
2024-06-21 15:58:05 -0400

    Delete eval.sh

--- eval.sh
+++
@@ -1,5 +0,0 @@
-accelerate launch --num_processes 2 --no_python \
-alpaca_eval evaluate_from_model \
-  --model_configs llama2_7B_45B_sparse50_LR2e-4_GC2_E2_70_LR2e-4_E2_GC2_Tdense_LR1e-4_E1_quant \
-  --annotators_config llama-2-7b-chat-hf \
-  --max_instances 128
